85482a9f23b7d16ddd2d2a624bbce9980db81c78
Vladislav Goffman <vgoffman@me.com>
2018-06-09 10:47:55 -0400

    Update installerPi3.sh

--- installerPi3.sh
+++ installerPi3.sh
@@ -36,7 +36,7 @@ make install
 ldconfig
 
 #Install nodejs
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #Install and Configure Asterisk
